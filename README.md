2fa73580b630d937b3c5d1df6de9fef641a003cd
Misha Panshenskov <misha.panshenskov@autodesk.com>
2021-09-28 11:40:35 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Autodesk Authorization Header Extractor
+
+**This is NOT an official Autodesk product, and is not related to Autodesk in any way**
+
+Chrome extension that extracts the authorization header from open BIM 360 or Autodesk Construction Cloud tabs. These tokens can later be used to manually send requests to Forge. To add the extension to your browser, visit the [Chrome web store]
---
+++ src/background.js
@@ -0,0 +1,20 @@
+
+chrome.webRequest.onSendHeaders.addListener(
+  function (info) {
+    if (info.requestHeaders) {
+      for(var i = 0; i < info.requestHeaders.length; i++) {
+        if(info.requestHeaders[i].name == 'Authorization' || info.requestHeaders[i].name == 'authorization') {
+          console.log(info.requestHeaders[i].value);
+          chrome.storage.local.set({ 'tokenObj': { 'token': info.requestHeaders[i].value, 'date': new Date().toLocaleTimeString() } });
+          break;
+        }
+      }      
+    }
+  },
+  // filters
+  {
+    urls: [
+      "https://*.autodesk.com/*",
+    ]
+  },
+  ["requestHeaders"]);
